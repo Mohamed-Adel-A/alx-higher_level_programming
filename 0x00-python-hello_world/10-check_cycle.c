d8619dcc4cfe8274b496311cbd3f81939f2fbfe5
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-06 04:28:15 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,30 +1,30 @@
-
-
+#include "lists.h"
 /**
  * check_cycle - check_cycle
  * @list: list
  * @node: node to be checked
  * @i: index
  *
- * Return: 1 if there is loop, 0 if not
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
 
 	int idx = 0, occurrence = 0;
-  listint_t *ptr_lst;
-	/*printf("%i" , i);*/
-	while (idx != i + 1)
+	listint_t *node, *head = list;
+
+
+	while (head != NULL)
 	{
-		/*printf("   [%p] %i : %i (occ : %i)\n", (void *)head, idx, i, occurrence);*/
-		if (head == node)
-			occurrence++;
+		node = head->next;
+		while (node !=NULL)
+		{
+			if (head == node)
+				return (1);
+			node = node->next;
+		}
 		head = head->next;
-		idx++;
 	}
-	if (occurrence > 1)
-	{
-		return (1);
-	}
+
 	return (0);
 }
