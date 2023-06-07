c53876026393f4d85a7c737835cba205668e78e2
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-07 04:33:08 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     i = 0
-    while str[i] != '\0':
-        if (ord(str) >= 97 and ord(str) <= 122):
+    for c in str:
+        if (ord(c) >= 97 and ord(c) <= 122):
             c = ord(str[i]) - 32
         else:
             c = ord(str[i])
