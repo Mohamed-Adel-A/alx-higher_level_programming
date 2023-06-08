3f9e69d1658bdeb948997ecabe908abbb1b6c2fc
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-08 07:17:59 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     args_len = len(argv)
     sum = 0
     if args_len > 1:
-        for i in ragne(1, args_len):
+        for i in range(1, args_len):
             sum = sum + int(argv[i])
     print("{:d}".format(sum))
