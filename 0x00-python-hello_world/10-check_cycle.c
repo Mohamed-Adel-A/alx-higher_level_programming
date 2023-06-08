095ae6164924b6c1ef706b388866d8bcbdd30902
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-08 14:26:12 +0300

    Update 10-check_cycle.c ==

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,16 +10,13 @@ int check_cycle(listint_t *list)
 {
 	listint_t *node;
 
-	printf("====start====\n");
 	if (list == NULL)
 		return (0);
 	while (list != NULL)
 	{
-		printf("\n(l= %d)\n", list->n);
 		node = list->next;
 		while (node != NULL)
 		{
-			printf("(n= %d)\n", node->n);
 			if (list == node)
 				return (1);
 			node = node->next;
