1ab3d44e235d7e0570355cac686eaf6e3047eba1
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-13 00:25:09 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,7 @@ def no_c(my_string):
     new_string = ""
     for c in my_string:
         if (c == 'C' or c == 'c'):
-            c = None
-    return (my_string)
+            pass
+        else:
+            new_string += c
+    return (new_string)
