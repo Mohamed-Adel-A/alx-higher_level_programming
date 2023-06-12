08be7898a64a55444a0bc75e1a65a1fe8d1cb336
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-13 00:32:53 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(e), end="")
                 if(e != r[-1]):
                     print(" ")
-        print("")
+        print()
