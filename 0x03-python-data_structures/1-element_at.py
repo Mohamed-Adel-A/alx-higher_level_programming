ecb57bbd0665c2684543b30dcea649e7c0801b15
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-12 23:52:09 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if (idx < 0):
         return (None)
     len_list = len(my_list)
-    if (idx > len_list):
+    if (idx >= len_list):
         return (None)
     return(my_list[idx])
