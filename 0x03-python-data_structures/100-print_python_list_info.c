233e9f5437777043f648e2071254e961e8a4e6c4
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-13 05:40:11 +0300

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,9 @@
-#include <object.h>
+/*#include <object.h>*/
 #include <Python.h>
 #include <listobject.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * print_python_list_info - print_python_list_info
