9279de0f78f1a64c1a06086afb6d9affa217fa0b
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 01:22:17 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if (roman_string is None or type(roman_string) != str):
         return (0)
-    letters_values = {"I": 1, "V": 5, "X": 10, "L": 50,\
+    letters_values = {"I": 1, "V": 5, "X": 10, "L": 50,
                       "c": 100, "D": 500, "M": 1000}
     sum = 0
     for c in roman_string:
