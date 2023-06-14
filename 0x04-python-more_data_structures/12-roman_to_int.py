f441da6e804d19e02b014579729bcb4921788696
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 01:17:17 +0300

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    letters_values = {"I" : 1, "V" : 5, "X" : 10, "L" : 50\
+                     "c": 100, "D" : 500, "M" : 1000}
+    sum = 0
+    for c in roman_string:
+        sum += letters_values[c]
+    
