9e5ea7506ea4cc04fb768e03ac4b939dd0600008
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 01:07:20 +0300

    Create 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if (a_dictionary):
+        best = 0
+        best_key
+        for k, v in a_dictionary.items():
+            if v > best:
+                best = v
+                best_key = k
+        return (best_key)
+    else:
+        return (None)
