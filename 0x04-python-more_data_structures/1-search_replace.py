d76f04426c864416acd0794b1932623e5fc3eca9
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 00:16:39 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if (my_list and search and replace):
-        # new_list = [replace if e == search else e for e in my_list]
-        new_list = []
-        for e in my_list:
-            if e == search:
-                new_list.append(replace)
-            else:
-                new_list.append(e)
-        return (new_list)
+    # new_list = [replace if e == search else e for e in my_list]
+    new_list = []
+    for e in my_list:
+        if e == search:
+            new_list.append(replace)
+        else:
+            new_list.append(e)
+    return (new_list)
