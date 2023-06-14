4608a2dcfb46bb1e6229f7e442a211a6e0c78030
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 00:07:29 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if (matrix):
-        new_matrix= [[e ** 2 for e in r] for r in matrix]
+        new_matrix = [[e ** 2 for e in r] for r in matrix]
         return (new_matrix)
