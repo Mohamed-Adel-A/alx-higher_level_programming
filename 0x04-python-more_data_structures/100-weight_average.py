212e916dea3b243f4ec5e58109af98d16d7f0df5
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 01:34:06 +0300

    Create 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if (len(my_list) == 0):
+        return (0)
+
+    weight_sum = 0
+    score_sum = 0 
+    for t in my_list:
+        weight_sum += (t[0] * t[1])
+        score_sum += t[0]
+    return (weight_sum / score_sum)
