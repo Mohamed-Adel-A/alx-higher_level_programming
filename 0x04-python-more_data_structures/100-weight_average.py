1412a19cc0e74eeff12eca73e4240019017bb411
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 01:36:34 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if (not my_list):
-        return (0)
-
+    if not my_list:
+        return 0
     weight_sum = 0
-    score_sum = 0 
+    score_sum = 0
     for t in my_list:
         weight_sum += (t[0] * t[1])
         score_sum += t[0]
