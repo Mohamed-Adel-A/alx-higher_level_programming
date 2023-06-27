931d6941eae41a6548152d92d8642d3896bde7d1
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-27 03:49:39 +0300

    Create 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    numbers_printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".fromat(mylist[i]), end="")
+            numbers_printed += 1
+        except (TypeError, ValueError):
+            continue
+        print("")
+        return (numbers_printed)
+      
