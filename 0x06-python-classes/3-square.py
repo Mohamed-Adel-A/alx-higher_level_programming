43bdddffa63d068b5f0c4d76618f92ed58005e91
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-28 05:13:34 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,6 +13,7 @@ class Square:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
+
     def area(self):
         """ return instance area """
         return (self.size * self.size)
