887f027e4ed9152d300eb922ff85ce27e191c566
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-28 19:31:59 +0300

    Create 100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" define Singly linked list """
+
+class Node:
+    """ define node class """
+    def __init__(self, data, next_node=None):
+        """ init node calss """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
