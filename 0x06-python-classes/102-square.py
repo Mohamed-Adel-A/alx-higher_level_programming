11b87a09cdcb532bca58bbba6235d23f80136ea9
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-28 12:33:52 +0300

    Create 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Defining a square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """ init a square instance """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """ Return the area of the square."""
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
+
+    def __le__(self, other):
+        return self.size <= other.size
+
+    def __gt__(self, other):
+        return self.size > other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
