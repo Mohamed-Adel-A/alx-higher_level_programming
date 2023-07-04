c6fd407cd9a362e6514a666399b0435796bb4bff
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-05 02:29:38 +0300

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.times = getattr(magic_string, 'times', 0) + 1
+    magic_string.times = getattr(magic_string, 'times', 0)
     return ("BestSchool, " * (magic_string.times) + "BestSchool")
