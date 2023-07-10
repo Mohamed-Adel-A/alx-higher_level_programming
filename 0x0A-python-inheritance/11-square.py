3e7006faf1f71039173e01ca473969ddb8b55547
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-11 01:17:53 +0300

    Create 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 8. Rectangle """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ subcalss from Rectangle """
+    def __init__(self, size):
+        """
+        init Rectangle instance
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
