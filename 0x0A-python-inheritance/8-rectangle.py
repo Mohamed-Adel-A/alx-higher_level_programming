cef39c36a23bfbebbb26c88d131e3eac65d51c94
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-11 00:51:05 +0300

    Create 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 8. Rectangle """
+BaseGeometry =  __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ subcalss from BaseGeometry """
+    def __init__(self, width, height):
+        self.__width = width
+        self.integer_validator(self, "width", width)
+        self.__height = height
+        self.integer_validator(self, "height", height)
