6154874976761ac3f5d310ffc0fc3d09471c06f3
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-12 03:12:46 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,6 +16,7 @@ class Student:
         that retrieves a dictionary representation of a Student instance
         """
         if ((type(attrs) == list) and (all(type(e) == str for e in attrs))):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+            return {key: getattr(self, key) for
+                    key in attrs if hasattr(self, key)}
         else:
             return (self.__dict__)
