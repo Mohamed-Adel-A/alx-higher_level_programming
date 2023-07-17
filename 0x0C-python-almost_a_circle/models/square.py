e5ed3b53972796e7af7b8ea54cfea447876ddc8f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-17 05:41:42 +0300

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+models/square.py
+square class model
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        initaializing square instance
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ instance string representation """
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.width))
