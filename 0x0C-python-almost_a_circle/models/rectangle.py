f53415a7a381e993b9f72378399ebd7c17303636
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-17 05:21:45 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,6 +71,9 @@ class Rectangle(Base):
 
     def display(self):
         """  prints in stdout the Rectangle instance with the character # """
+        if (self.width == 0 or self.height == 0):
+            print("")
+            return
         for i in range(self.y):
             print("")
         for i in range(self.height):
@@ -84,3 +87,41 @@ class Rectangle(Base):
         """ string representing the instance """
         return ("[Rectangle] ({}) {}/{} - {}/{}"
                 .format(self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args, **kwargs):
+        """
+        Update the class Rectangle
+        that assigns an argument to each attribute
+        1st argument should be the id attribute
+        2nd argument should be the width attribute
+        3rd argument should be the height attribute
+        4th argument should be the x attribute
+        5th argument should be the y attribute
+        """
+        if len(args) != 0:
+            i = 0
+            for arg in args:
+                if (i == 0):
+                    self.id = arg
+                elif (i == 1):
+                    self.width = arg
+                elif (i == 2):
+                    self.height = arg
+                elif (i == 3):
+                    self.x = arg
+                elif (i == 4):
+                    self.y = arg
+                i += 1
+        if len(kwargs) != 0:
+            for key, value in kwarg.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+            
