22ebe16f53b5e8e19f60fb86cfd871047138c463
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-14 16:25:45 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,9 @@
 def add_integer(a, b=98):
     """Add two number together"""
     if (type(a) is not int) and (type(a) is not float):
-        print("a must be an integer")
+        raise TypeError("a must be an integer")
         return
     if (type(b) is not int) and (type(b) is not float):
-        print("b must be an integer")
+        raise TypeError("b must be an integer")
         return
     return (int(a) + int(b))
