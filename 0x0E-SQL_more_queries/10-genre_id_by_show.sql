28d026798f02b5b67b12aabdb456bd71769774fb
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-17 05:21:57 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked.
 SELECT ts.title, tsg.genre_id 
-  FROM tv_shows as ts
-  INNER JOIN tv_show_genres as tsg
+  FROM tv_shows ts
+  INNER JOIN tv_show_genres tsg
   ON ts.id = tsh.show_id 
   ORDER BY ts.title ASC, 
            tsg.genre_id ASC;
