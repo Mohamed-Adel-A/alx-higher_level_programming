8f0b7ba5cc196c30e068b7a7ffcf60513db6d702
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-17 05:22:25 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- that have at least one genre linked.
 SELECT ts.title, tsg.genre_id 
   FROM tv_shows ts
-  INNER JOIN tv_show_genres tsg
+  JOIN tv_show_genres tsg
   ON ts.id = tsh.show_id 
   ORDER BY ts.title ASC, 
            tsg.genre_id ASC;
