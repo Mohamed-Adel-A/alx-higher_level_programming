0035d90f11a61c5e8a128b90277f6369824aaeb1
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-13 01:19:46 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 
+function factorial (a) {
+  if (isNaN(a)) {
+    console.log(1);
+  } else {
+    let fact = 1;
+    for (let i = a; i > 0; i--) {
+      fact *= i;
+    }
+    console.log(a);
+  }
+}
+
 const arg = process.argv[2];
 
-if (isNaN(arg)) {
-  console.log(1);
-} else {
-  let fact = 1;
-  for (let i = arg; i > 0; i--) {
-    fact *= i;
-  }
-  console.log(fact);
-}
-  
+factorial(arg);
