d7018bf01029efd7d6e90d0dd0a0cceba203ea72
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-13 01:37:09 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const argCount = process.argv.length;
-if (argCount <= 2) {
+if (argCount <= 3) {
   console.log('0');
 } else {
   const args = process.argv.slice(2);
   const nums = args.map(Number);
 
-  nums.sort(function (a, b) { return (b - a);});
+  nums.sort(function (a, b) { return (b - a); });
 
   console.log(nums[1]);
 }
