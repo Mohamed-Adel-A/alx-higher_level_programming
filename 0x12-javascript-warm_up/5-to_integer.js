63c6162ca3ec4d9e9e45c3c04ba42e16db7ace2d
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-13 00:49:11 +0300

    Create 5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const number = process.argv[2];
+
+if (isNaN(number) || number === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(number));
+}
+
