f3ea12eca8fc2c337b51111f7a2f3711011ddebe
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-16 23:29:05 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 contains the class definition of a City
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
