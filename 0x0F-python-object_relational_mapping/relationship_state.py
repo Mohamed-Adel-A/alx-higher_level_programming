b1ef39cbc5341a0e5605e7474be046ed80e6d642
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-17 00:20:50 +0300

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, unique=True,
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="states")
+    cities = relationship("City", backref="states")
