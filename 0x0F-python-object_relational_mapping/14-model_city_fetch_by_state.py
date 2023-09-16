c9ad096b97839de77b016d3cab3fe2ff3da10a96
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-16 23:25:48 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,7 +6,7 @@ from the database hbtn_0e_14_usa:
 
 import sys
 from sqlalchemy import create_engine
-from model_state import Base, State
+from model_city import Base, City
 from sqlalchemy.orm import sessionmaker
 
 
