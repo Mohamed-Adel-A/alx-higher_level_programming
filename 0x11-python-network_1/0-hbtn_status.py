f2da385bd47604c2f7eced1ad563a5738cad47ad
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-30 00:44:49 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 
 if __name__ == "__main__":
     from urllib import request
-    
+
     r = request.urlopen("https://alx-intranet.hbtn.io/status")
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         data = response.read()
