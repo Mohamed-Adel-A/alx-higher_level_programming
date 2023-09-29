74700de742c2a945d7179937d6da839a26da1d31
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-29 03:22:24 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,3 +13,7 @@ def find_peak(list_of_integers):
         return peek
     if len(list_of_integers) == 0:
         return peek
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
