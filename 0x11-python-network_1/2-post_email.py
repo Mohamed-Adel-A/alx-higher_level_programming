8d1cd25a6fbded5b7700cd08627857d47a6ff5c3
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-30 00:59:53 +0300

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+
+if __name__ == "__main__":
+    from urllib import request
+    import sys
+    from urllib import parse
+
+    values = {"email": sys.argv[1]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.
+    with request.urlopen(sys.argv[1], data) as response:
+        result = response.read().decode("utf-8")
+        print(result)
