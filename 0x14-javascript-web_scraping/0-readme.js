990981c02832bd7d9420c74761404ef376f63840
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-25 01:08:44 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-/* 
-a script that reads and prints the content of a file.
-- The first argument is the file path
-- The content of the file must be read in utf-8
-- If an error occurred during the reading, print the error object
-*/
+// a script that reads and prints the content of a file.
+// - The first argument is the file path
+// - The content of the file must be read in utf-8
+// - If an error occurred during the reading, print the error object
+
 const fs = require('fs');
 const filename = process.argv[2];
 
