9137b73bebc8a1010aa865dafbf5e49dc8069488
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-25 01:28:12 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,6 @@
 
 const request = require('request');
 const url = process.argv[2];
-const process = require('process');
 
 request.get(url).on('response', function (response) {
   console.log('code: ' + response.statusCode);
