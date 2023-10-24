64648508221c7dd948c813eeee707a4d0a9f3ce0
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-25 01:13:14 +0300

    Create 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script that writes a string to a file.
+// - The first argument is the file path
+// - The second argument is the string to write
+// - The content of the file must be written in utf-8
+// - If an error occurred during while writing, print the error object
+
+const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filename, content, error => {
+  if (error) console.log(error);
+});
