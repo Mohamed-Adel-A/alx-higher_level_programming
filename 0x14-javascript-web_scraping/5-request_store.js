e534f5f6f065708f5a2d30b7958acd0bd1cf75c4
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-25 01:58:57 +0300

    Create 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that gets the contents of a webpage and stores it in a file.
+// - The first argument is the URL to request
+// - The second argument the file path to store the body response
+// - The file must be UTF-8 encoded
+// - You must use the module request
+
+const fs = require('fs');
+const request = require('request');
+const APIurl = process.argv[2];
+const filename = process.argv[3];
+
+request(APIurl).pipe(fs.createWriteStream(filename));
