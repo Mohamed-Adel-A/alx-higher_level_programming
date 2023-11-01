0a4639a4b3ba223f1fd0d84172909a15eeccf5b3
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-02 00:27:07 +0200

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 const url = "https://swapi-api.alx-tools.com/api/films/?format=json"
 $.get(url, function(data, status) {
-    for (item in data.results)
+    for (let item in data.results)
         $("UL#list_movies").append("<li>" + item.title + "</li>");
 });
