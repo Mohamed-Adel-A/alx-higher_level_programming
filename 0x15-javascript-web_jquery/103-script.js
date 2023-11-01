cd0cb4f2d45c21fb83a0cdb6f3f968c1ee908274
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-02 01:18:33 +0200

    Create 103-script.js

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+const url = "https://hellosalut.stefanbohacek.dev/";
+$("INPUT#btn_translate").click(function() {
+    lang = $("INPUT#language_code").val();
+    full_url = url + "?lang=" + lang;
+    $.get(full_url, function(data) {
+        $("DIV#hello").text(data.hello);
+    });
+});
+    
+$("INPUT#language_code").keypress(function() {
+    lang = $("INPUT#language_code").val();
+    full_url = url + "?lang=" + lang;
+    $.get(full_url, function(data) {
+        $("DIV#hello").text(data.hello);
+    });
+});
+    
+};
