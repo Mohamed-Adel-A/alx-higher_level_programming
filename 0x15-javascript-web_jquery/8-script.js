8f741a7ee67f0a8c4d4f16fd2f72e03a7cf20aa6
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-02 00:21:16 +0200

    Create 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+const url = "https://swapi-api.alx-tools.com/api/films/?format=json"
+$.get(url, function(data, status) {
+    for (item in data)
+        $("UL#list_movies").append("<li>" + item + "</li>");
+});
