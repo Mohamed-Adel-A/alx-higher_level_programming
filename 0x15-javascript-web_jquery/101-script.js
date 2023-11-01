b5dd1cac0de0a33f7ead62f2d3b80e900a1a5803
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-02 00:55:42 +0200

    Update 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
 $("DIV#add_item").click(function() {
     $("UL.my_list").append("<li>Item</li>");
 });
@@ -9,3 +10,4 @@ $("DIV#remove_item").click(function() {
 $("DIV#clear_list").click(function() {
     $("UL.my_list").empty();
 });
+};
