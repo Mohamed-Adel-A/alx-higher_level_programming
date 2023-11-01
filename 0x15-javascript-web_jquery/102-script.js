13b7d432031125aac9c0493f6b26168175aa40a8
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-02 01:12:33 +0200

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -3,6 +3,7 @@ const url = "https://hellosalut.stefanbohacek.dev/";
 $("INPUT#btn_translate").click(function() {
     lang = $("INPUT#language_code").val();
     full_url = url + "?lang=" + lang;
+    alert(full_url);
     $get(full_url, function(data) {
         $("DIV#hello").text(data.hello);
     });
